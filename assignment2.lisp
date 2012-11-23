d1d5ebf1ec91391e0b92280f0b5f586250870f93
Emil <erif89@gmail.com>
2012-11-23 13:55:57 +0100

    Initial commit

---
+++ assignment2.lisp
@@ -0,0 +1,13 @@
+;;; Advanced Functional Programming, Assignment 2 - Lisp
+;;; Authors:
+;;; - Carl Carenvall <caca7037@student.uu.se>
+;;; - Emil Wall <emwa3503@student.uu.se>
+
+;;
+;; Creates a new empty dictionary with test being the equality function to be
+;; used for keys. If test is not given, EQ is used.
+;;
+(defun create-dictionary (&key test)
+    "Returns a new dict with test as equality function"
+    nil)  ; TODO implement...
+
