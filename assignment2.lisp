fc434baf159c3dd0b4a0a05192617c281271b635
Emil <erif89@gmail.com>
2012-11-23 16:46:35 +0100

    let instead of setf?

--- assignment2.lisp
+++ assignment2.lisp
@@ -19,7 +19,7 @@
 ;;
 (defun lookup (key dict &key default)
     "Returns dict[key], or default/nil if no such value exists"
-    (if (setf value (gethash key dict)) ; then
+    (let value (gethash key dict) (if (value) ; then
         value ; else
         default))
 
